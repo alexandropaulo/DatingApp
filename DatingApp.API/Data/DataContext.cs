0281d6821cdf575cf5d6cf998174e3b60b2f67cd
alexandropaulo <alexandro_s@hotmail.com>
2021-01-14 14:34:54 -0300

    End of Section 3

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -6,5 +6,6 @@ namespace DatingApp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options){ }
         public DbSet<Models.Value> Values { get; set; }
+        public DbSet<Models.User> Users { get; set; }
     }
 }
